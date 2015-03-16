adffc9884a2b223105ac1de12fa93d1c5643571c
Alexa Villaume <avillaum@ucsc.edu>
2015-03-16 12:53:56 -0700

    have sorted out the temperature stucture stuff at this point. still need to get all the units to work out

--- agn_disks.py
+++ agn_disks.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from astropy import units as u
 
 
-light_speed = 2.9979e11  # cm s^-1
+light_speed = 2.9979e8  # m s^-1
 
 def readin_data(fname):
     """
@@ -20,7 +20,7 @@ def readin_data(fname):
     """
     return 0
 
-def generate_spec(nu1, nu2):
+def generate_spec(nu1, nu2, dist):
     """
     Generate a fake spectrum under the assumptions of
     the standard accretion disk model.
@@ -32,9 +32,9 @@ def generate_spec(nu1, nu2):
     """
 
     freq = np.linspace(nu1, nu2, 1e4)
-    return [freq, (freq**(1./3.))]
+    return [freq, (freq**(1./3.)) * 4*np.pi*dist**2]
 
-def compute_planck_freq(freq, temp, dist):
+def compute_planck_freq(freq, temp):
     """
     freq is a numpy array. Constants in SI units.
     Returning units of flux density (jansky). Assuming that the
@@ -44,13 +44,10 @@ def compute_planck_freq(freq, temp, dist):
     planck_c = 6.626e-34    # J s
     boltzmann_c = 1.38e-23  # J K^-1
 
-    theta = 1e-3 # for now until I can get real values
-
-    spec_radiance = ((2*planck_c*freq**3)/(light_speed**2))*\
-                    (1/np.expm1((planck_c*freq)/(boltzmann_c*temp)))
+    spec_radiance = (8.*np.pi*planck_c*freq**3.)/(light_speed**2)*\
+                    1./(np.exp(planck_c*freq/(boltzmann_c*temp))-1)
 
     return spec_radiance
-    #return spec_radiance*4*np.pi*dist**2
 
 def sum_planck_curves():
     """
@@ -59,34 +56,69 @@ def sum_planck_curves():
     """
     return 0
 
+def check_planck():
+    # http://www.astro.umd.edu/~cychen/MATLAB/ASTR120/Blackbody2.html
+    temps = np.linspace(3000, 9000, 3)
+    comp_spec_freq = np.linspace(light_speed/1e-9, light_speed/3000e-9, 1e6)
+    #comp_spec_freq = np.linspace(2000*light_speed*8.0655e-5, light_speed/1e-6, 1e2)
+
+    comp_spec_flux = np.zeros((len(comp_spec_freq), len(temps)))
+    for i, temp in enumerate(temps):
+        comp_spec_flux[:,i] = compute_planck_freq(comp_spec_freq, temp)
+
+    plt.plot(comp_spec_freq, comp_spec_flux[:,0])
+    plt.plot(comp_spec_freq, comp_spec_flux[:,1])
+    plt.plot(comp_spec_freq, comp_spec_flux[:,2])
+    plt.plot(comp_spec_freq, np.sum(comp_spec_flux, axis=1), color='k')
+
+    plt.xlim(1e14,9e14)
+    plt.show()
+
+    sys.exit()
+
 def temp_struc(radii, power):
-    return radii**power
+    """
+    Scale the temperatures to 1300 K at
+    inner disk
+    """
+    temp_struct = radii**power
+    scale = 1300 / temp_struct[0]
+
+    return temp_struct * scale
+
 
 if __name__ == '__main__':
     check = False
     if check:
         check_planck()
 
-    r_in = (40*(1./365))*9.4606e17
-    r_out = (150*(1./365))*9.4606e17
+    dist = 2.854e27 # in cm, usuing NGC 5548 (92.5 Mpc)
+    std_spec = generate_spec(2000*light_speed*8.0655e-5, light_speed/1e-6, dist)
 
-    std_spec = generate_spec(2000*light_speed*8.0655e-5, light_speed/1e-6)
+    r_in = (40*(1./365))
+    r_out = (1000*(1./365))
+    disk_radii = np.logspace(r_in, r_out) * 9.4606e17
 
     # Make computed SED
     comp_spec_freq = np.linspace(2000*light_speed*8.0655e-5, light_speed/1e-6, 1e2)
-    disk_radii = np.linspace(r_in, r_out)
-
-    comp_spec_flux = []
     temps = temp_struc(disk_radii, -3./4.)
-    for nu in comp_spec_freq:
-        lum = []
-        for temp in temps:
-            lum.append(compute_planck_freq(temp, nu, 1000)*np.pi)
-        comp_spec_flux.append(sum(lum)*1e59)
 
-    plt.plot(std_spec[0], std_spec[1], ls='none', marker='o', color='k', label='Input, $F^{1/3}$')
-    plt.plot(comp_spec_freq, comp_spec_flux, ls='-', color='r', label='Computed')
+    comp_spec_flux = np.zeros((len(comp_spec_freq), len(temps)))
+    for i, temp in enumerate(temps):
+        if i == 0:
+            comp_spec_flux[:,i] = compute_planck_freq(comp_spec_freq, temp)**2*np.pi*(disk_radii[i+1] - disk_radii[i])
+        else:
+            comp_spec_flux[:,i] = compute_planck_freq(comp_spec_freq, temp)**2*np.pi*(disk_radii[i] - disk_radii[i-1])
+
+    total_comp_flux = np.sum(comp_spec_flux, axis=1)/(4*np.pi*dist**2)
+
+    plt.plot(std_spec[0], std_spec[1], ls='-', color='k', label='Input, $F^{1/3}$')
+    plt.plot(comp_spec_freq, comp_spec_flux, ls='--', color='r', label='')
+    plt.plot(comp_spec_freq, total_comp_flux, ls='-', color='r', label='Computed')
+    plt.xlabel(r'$\nu$', fontsize=20)
+    plt.ylabel('Flux', fontsize=20)
     plt.legend()
+    #plt.xlim(10e14, 10e17)
     plt.xscale('log')
     plt.yscale('log')
     plt.show()
